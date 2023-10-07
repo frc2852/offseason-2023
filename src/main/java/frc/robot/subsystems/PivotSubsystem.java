bada19b6f165b6f642050301df6e745cded35c7c
Krystian Booker <krystian.booker@gmail.com>
2023-10-07 14:18:27 -0400

    Pivot PID tuning, limit switches

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -4,52 +4,83 @@
 
 package frc.robot.subsystems;
 
+import java.util.logging.LogManager;
+
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxLimitSwitch;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.util.LoggingManager;
+import frc.robot.util.LoggingManager.MessageType;
 
 public class PivotSubsystem extends SubsystemBase {
 
 	private final CANSparkMax pivotLeader;
-	private final AbsoluteEncoder pivotEncoder;
+	private AbsoluteEncoder pivotEncoder;
 
 	private final CANSparkMax pivotFollower;
 	private final SparkMaxPIDController pivotPIDController;
 
 	private double position = 0;
 
+	private final SparkMaxLimitSwitch forwardLimit;
+	private final SparkMaxLimitSwitch reverseLimit;
+
 	public PivotSubsystem() {
 		pivotLeader = new CANSparkMax(Constants.CanbusId.PIVOT_LEADER, MotorType.kBrushless);
 		pivotFollower = new CANSparkMax(Constants.CanbusId.PIVOT_FOLLOWER, MotorType.kBrushless);
 		pivotEncoder = pivotLeader.getAbsoluteEncoder(Type.kDutyCycle);
+		if (pivotEncoder == null) {
+			LoggingManager.log("Pivot encoder was null", MessageType.ERROR);
+			try {
+				Thread.sleep(1000);
+				pivotEncoder = pivotLeader.getAbsoluteEncoder(Type.kDutyCycle);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
 		pivotPIDController = pivotLeader.getPIDController();
+		forwardLimit = pivotLeader.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+		reverseLimit = pivotLeader.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+		forwardLimit.enableLimitSwitch(true);
+		reverseLimit.enableLimitSwitch(true);
+		SmartDashboard.putBoolean("Forward Limit Enabled", forwardLimit.isLimitSwitchEnabled());
+		SmartDashboard.putBoolean("Reverse Limit Enabled", reverseLimit.isLimitSwitchEnabled());
 
 		configureMotors();
 		configurePID();
 		burnFlash();
 
 		SmartDashboard.putNumber("Position", position);
+		SmartDashboard.putNumber("P Value", Constants.Pivot.P);
+		SmartDashboard.putNumber("I Value", Constants.Pivot.I);
+		SmartDashboard.putNumber("D Value", Constants.Pivot.D);
+		SmartDashboard.putNumber("IZone", Constants.Pivot.IZONE);
+		SmartDashboard.putNumber("FF Value", Constants.Pivot.FF);
+		SmartDashboard.putNumber("Min Output", Constants.Pivot.MIN_OUTPUT);
+		SmartDashboard.putNumber("Max Output", Constants.Pivot.MAX_OUTPUT);
 	}
 
 	private void configureMotors() {
 		pivotLeader.restoreFactoryDefaults();
 		pivotLeader.setIdleMode(IdleMode.kBrake);
 		pivotLeader.setInverted(false);
-		// pivotLeader.setSmartCurrentLimit(20);
 
 		pivotFollower.restoreFactoryDefaults();
 		pivotFollower.follow(pivotLeader);
 		pivotFollower.setIdleMode(IdleMode.kBrake);
 		pivotFollower.setInverted(true);
-		// pivotFollower.setSmartCurrentLimit(20);
 	}
 
 	private void configurePID() {
@@ -64,11 +95,6 @@ public class PivotSubsystem extends SubsystemBase {
 		pivotPIDController.setFF(Constants.Pivot.FF);
 		pivotPIDController.setOutputRange(Constants.Pivot.MIN_OUTPUT, Constants.Pivot.MAX_OUTPUT);
 		pivotPIDController.setFeedbackDevice(pivotEncoder);
-
-		// pivotPIDController.setSmartMotionAccelStrategy(null, 0)
-		// pivotPIDController.setSmartMotionAllowedClosedLoopError(position, 0)
-		// pivotPIDController.setSmartMotionMaxAccel(position, 0)
-		// pivotPIDController.setSmartMotionMaxVelocity(position, 0)
 	}
 
 	private void burnFlash() {
@@ -87,5 +113,25 @@ public class PivotSubsystem extends SubsystemBase {
 		SmartDashboard.putNumber("PositionConversion", pivotEncoder.getPositionConversionFactor());
 		position = SmartDashboard.getNumber("Position", 0);
 		setAngle(position);
+
+		// Fetch PID values from SmartDashboard
+		double pValue = SmartDashboard.getNumber("P Value", Constants.Pivot.P);
+		double iValue = SmartDashboard.getNumber("I Value", Constants.Pivot.I);
+		double dValue = SmartDashboard.getNumber("D Value", Constants.Pivot.D);
+		double izoneValue = SmartDashboard.getNumber("IZone", Constants.Pivot.IZONE);
+		double ffValue = SmartDashboard.getNumber("FF Value", Constants.Pivot.FF);
+		double minOutput = SmartDashboard.getNumber("Min Output", Constants.Pivot.MIN_OUTPUT);
+		double maxOutput = SmartDashboard.getNumber("Max Output", Constants.Pivot.MAX_OUTPUT);
+
+		// Update PID controller with the fetched values
+		pivotPIDController.setP(pValue);
+		pivotPIDController.setI(iValue);
+		pivotPIDController.setD(dValue);
+		pivotPIDController.setIZone(izoneValue);
+		pivotPIDController.setFF(ffValue);
+		pivotPIDController.setOutputRange(minOutput, maxOutput);
+
+		SmartDashboard.putBoolean("Forward Limit Switch", forwardLimit.isPressed());
+		SmartDashboard.putBoolean("Reverse Limit Switch", reverseLimit.isPressed());
 	}
 }
